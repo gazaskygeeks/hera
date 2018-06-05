cf88905008a47828a10e360f1ff9d0ffb269c918
noureldean <noureldean.saed@gmail.com>
2018-06-06 01:42:06 +0300

    -

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Navbar from '../components/global/navbar';
 import LandingPage from '../containers/landingPageContainer';
 
 const App = () => (
   <BrowserRouter>
     <div className="app">
+      <Navbar />
       <Switch>
         <Route exact path="/" component={LandingPage} />
       </Switch>
---
+++ src/client/components/global/navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.less';
+
+const Navbar = () => (
+  <div className="navbar">
+    <i className="fas fa-circle-notch navbar__swapper" />
+    <ul>
+      <li><input type="text" placeholder="Search..." /></li>
+      <li>a</li>
+      <li>a</li>
+      <li>a</li>
+      <li>a</li>
+    </ul>
+  </div>
+);
+
+export default Navbar;
